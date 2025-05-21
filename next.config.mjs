bd164f537d9595b237381eb4e2b372efb908f9f2
agripaa <agrieva.xananda08@gmail.com>
2025-05-21 15:19:42 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
-    // output: "export",
     images: {
         unoptimized: true, // Agar gambar bisa di-load di Netlify tanpa optimasi Next.js
       },
